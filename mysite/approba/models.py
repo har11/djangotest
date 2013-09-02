bf5046eeb25e2c161a226ba1698014fa394daada
har11 <har11&freemail.hu>
2013-09-02 10:09:58 +0200

    user_id added to approbe_machine table, but I could not sync into the DB. Login on machine page has not resolved yet

--- mysite/approba/models.py
+++ mysite/approba/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 import datetime
 from django import forms
+import django.contrib.auth.models
 
 class Machine(models.Model):
 	machine_name = models.CharField(max_length=100, unique=True)
 	machine_description = models.CharField(max_length=1000)
 	created_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
+	created_by = models.ForeignKey(django.contrib.auth.models.User,related_name='user_created_by')
 	updated_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
+	updated_by = models.ForeignKey(django.contrib.auth.models.User,related_name='user_updated_by')
 	
 class MachineForm(forms.ModelForm):
 	machine_name = forms.CharField(max_length=100)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'polls',
     'misiproba',
     'approba',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
