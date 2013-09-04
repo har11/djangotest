27d888a878e20464a60c464c1cd5521843179dc7
har11 <har11&freemail.hu>
2013-09-04 09:42:08 +0200

    machine_name and created_by id is unique together, different users can define the same name. Since the unique constraint exception is not handled by django, I will have to resolve it in code

--- mysite/approba/models.py
+++ mysite/approba/models.py
@@ -4,12 +4,15 @@ from django import forms
 import django.contrib.auth.models
 
 class Machine(models.Model):
-	machine_name = models.CharField(max_length=100, unique=True)
+	machine_name = models.CharField(max_length=100)
 	machine_description = models.CharField(max_length=1000)
 	created_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
 	created_by = models.ForeignKey(django.contrib.auth.models.User,related_name='user_created_by')
 	updated_at = models.DateTimeField(editable=False, default=datetime.datetime.now)
 	updated_by = models.ForeignKey(django.contrib.auth.models.User,related_name='user_updated_by')
+	
+	class Meta:
+		unique_together = ("machine_name", "created_by")
 	
 class MachineForm(forms.ModelForm):
 	machine_name = forms.CharField(max_length=100)
