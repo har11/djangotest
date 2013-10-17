75d5a58833b94c4c3bb28174c4f23a98235a1057
har11 <har11&freemail.hu>
2013-10-17 09:15:58 +0200

    filedoperation model added

---
+++ mysite/approba/migrations/0007_auto__add_fieldoperation.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'FieldOperation'
+        db.create_table(u'approba_fieldoperation', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('machine', self.gf('django.db.models.fields.related.ForeignKey')(related_name='fieldoperation_machine', to=orm['approba.Machine'])),
+            ('agrofield', self.gf('django.db.models.fields.related.ForeignKey')(related_name='fieldoperation_agrofield', to=orm['approba.AgroField'])),
+            ('operation', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('quantity', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('date', self.gf('django.db.models.fields.DateField')()),
+            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='fieldoperation_created_by', to=orm['auth.User'])),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('updated_by', self.gf('django.db.models.fields.related.ForeignKey')(related_name='fieldoperation_updated_by', to=orm['auth.User'])),
+        ))
+        db.send_create_signal(u'approba', ['FieldOperation'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'FieldOperation'
+        db.delete_table(u'approba_fieldoperation')
+
+
+    models = {
+        u'approba.agrofield': {
+            'Meta': {'unique_together': "(('name', 'created_by'),)", 'object_name': 'AgroField'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'agrofield_created_by'", 'to': u"orm['auth.User']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'updated_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'agrofield_updated_by'", 'to': u"orm['auth.User']"})
+        },
+        u'approba.fieldoperation': {
+            'Meta': {'object_name': 'FieldOperation'},
+            'agrofield': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fieldoperation_agrofield'", 'to': u"orm['approba.AgroField']"}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fieldoperation_created_by'", 'to': u"orm['auth.User']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'machine': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fieldoperation_machine'", 'to': u"orm['approba.Machine']"}),
+            'operation': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'quantity': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'updated_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'fieldoperation_updated_by'", 'to': u"orm['auth.User']"})
+        },
+        u'approba.machine': {
+            'Meta': {'unique_together': "(('machine_name', 'created_by'),)", 'object_name': 'Machine'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'user_created_by'", 'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'machine_description': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'machine_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'updated_by': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'user_updated_by'", 'to': u"orm['auth.User']"})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['approba']
